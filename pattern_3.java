d13963616f466bb64820ec730beba092e2e0b272
Raju VIshawakarma <rajuvish547@gmail.com>
2026-04-25 00:52:13 +0530

    Pattern 3

---
+++ pattern_3.java
@@ -0,0 +1,16 @@
+public class pattern_3 {
+    public static void main(String[] args){
+    // int i, n=5;
+        for(int i=5; i>=1; i--){
+            for(int j=i; j>=1; j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+// 54321
+// 4321
+// 321
+// 21
+// 1
